093de689693862300af916c70092e4425e790914
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-03 14:52:20 +0000

    Update src/modelling/decompose.py: In Progress

--- src/modelling/decompose.py
+++ src/modelling/decompose.py
@@ -12,8 +12,14 @@ class Decompose:
 
     def exc(self, data: pd.DataFrame):
 
-        components = stsl.STL(data['ln'], period=self.__arguments.get('seasons'),
+        frame = data.copy()
+
+        components: stsl.DecomposeResult = stsl.STL(frame['ln'], period=self.__arguments.get('seasons'),
                               seasonal=self.__decompose.get('smoother_seasonal'),
                               trend_deg=self.__decompose.get('degree_trend'),
                               seasonal_deg=self.__decompose.get('degree_seasonal'),
                               robust=True).fit()
+
+        frame['trend'] = components.trend
+        frame['residue'] = components.resid
+        frame['seasonal'] = components.seasonal
