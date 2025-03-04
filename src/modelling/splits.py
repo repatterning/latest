e8be6b9b188027789b9a8d43f9284fb82d6545a5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-04 12:13:17 +0000

    Update src/modelling/splits.py: In Progress

--- src/modelling/splits.py
+++ src/modelling/splits.py
@@ -1,7 +1,10 @@
 """Module splits.py"""
 import typing
-
+import os
 import pandas as pd
+
+import config
+import src.functions.streams
 
 
 class Splits:
@@ -16,6 +19,8 @@ class Splits:
         """
 
         self.__arguments = arguments
+        self.__configurations = config.Config()
+        self.__streams = src.functions.streams.Streams()
 
     def __include(self, blob: pd.DataFrame) -> pd.DataFrame:
         """
@@ -36,10 +41,21 @@ class Splits:
 
         return blob.copy()[-self.__arguments.get('ahead'):]
 
-    def exc(self, data: pd.DataFrame,) -> typing.Tuple[pd.DataFrame, pd.DataFrame]:
+    def __persist(self, blob: pd.DataFrame, string: str):
+        """
+
+        :param blob:
+        :param string:
+        :return:
+        """
+
+        self.__streams.write(blob=blob, path=os.path.join(self.__configurations.data_, string))
+
+    def exc(self, data: pd.DataFrame, code: str) -> typing.Tuple[pd.DataFrame, pd.DataFrame]:
         """
 
         :param data: The data set consisting of the attendance numbers of <b>an</b> institution/hospital.
+        :param code: An institution's identification code
         :return:
         """
 
