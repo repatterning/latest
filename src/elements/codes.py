9aacb884d6f35d4f0411b386276e63eaed8608ca
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-05 23:29:10 +0000

    Add src/elements/codes.py

---
+++ src/elements/codes.py
@@ -0,0 +1,18 @@
+import typing
+
+class Codes(typing.NamedTuple):
+    """
+    The data type class ⇾ Codes<br><br>
+
+    Attributes<br>
+    ----------<br>
+    <b>board</b> : str
+        A health board code<br>
+
+    <b>institution</b> : str
+        The code of an institution/hospital
+
+    """
+
+    board: str
+    institution: str
