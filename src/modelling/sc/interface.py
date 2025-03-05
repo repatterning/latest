42de2b5166c34f26e859cdfbaee9b1d6a5db99f3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-05 23:30:40 +0000

    Update src/modelling/sc/interface.py

--- src/modelling/sc/interface.py
+++ src/modelling/sc/interface.py
@@ -1,17 +1,35 @@
+"""Module interface.py"""
 import logging
-import pandas as pd
+
+import src.elements.codes as ce
+import src.elements.master as mr
+import src.modelling.sc.algorithm
+import src.modelling.sc.forecasts
+import src.modelling.sc.page
 
 
 class Interface:
 
     def __init__(self, arguments: dict):
+        """
+
+        :param arguments: A set of model development, and supplementary, arguments.
+        """
 
         self.__arguments = arguments
 
-    def exc(self, training: pd.DataFrame, testing: pd.DataFrame) -> bool:
+    def exc(self, master: mr.Master, code: ce.Codes) -> bool:
+        """
 
-        logging.info(self.__arguments)
-        logging.info(training.head())
-        logging.info(testing.head())
+        :param master: A named tuple consisting of an institutions training & testing data
+        :param code:
+        :return:
+        """
+
+        algorithm = src.modelling.sc.algorithm.Algorithm(arguments=self.__arguments)
+        system = algorithm.exc(data=master.training)
+        src.modelling.sc.page.Page().exc(system=system, code=code.institution)
+        src.modelling.sc.forecasts.Forecasts(data=master.training, testing=master.testing, system=system).exc(
+            arguments=self.__arguments, code=code)
 
         return True
