3054c36da9249ecba9b45f384a25f497a4196703
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-05 12:24:02 +0000

    Update src/modelling/sc/algorithm.py: The Seasonal Component algorithm for seasonal component modelling.  In progress.

--- src/modelling/sc/algorithm.py
+++ src/modelling/sc/algorithm.py
@@ -1,5 +1,38 @@
 
+import pandas as pd
+
+import statsmodels.tsa.forecasting.stl as tfc
+import statsmodels.tsa.arima.model as tar
+
+
 class Algorithm:
 
-    def __init__(self):
-        pass
+    def __init__(self, arguments: dict):
+
+        self.__arguments: dict = arguments
+        self.__sc: dict = self.__arguments.get('sc')
+
+    def exc(self, data: pd.DataFrame):
+        """
+        <b>References</b><br>
+        <a href="https://www.statsmodels.org/dev/generated/statsmodels.tsa.arima.model.ARIMA.html">
+        ARIMA (Autoregressive Integrated Moving Average)</a><br>
+        <a href="https://www.statsmodels.org/dev/generated/statsmodels.tsa.forecasting.stl.STLForecast.html">STLForecast</a><br><br>
+
+        :param data: The data of an institution, including the decompositions of its <i>ln(attendance numbers)</i> series.
+        :return:
+        """
+
+        tfc.STLForecast(
+            data[['seasonal']], tar.ARIMA,
+            model_kwargs=dict(
+                seasonal_order=(
+                    self.__sc.get('P'),
+                    self.__sc.get('D'),
+                    self.__sc.get('Q'),
+                    self.__sc.get('m')),
+                trend='c'),
+            seasonal=self.__sc.get('smoother_seasonal'),
+            seasonal_deg=self.__sc.get('degree_seasonal'),
+            trend_deg=self.__sc.get('degree_trend'),
+            robust=True)
