ba64a689943244ab815e759758299aa62c07ebd9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-06 15:18:47 +0000

    Update src/modelling/sc/page.py

--- src/modelling/sc/page.py
+++ src/modelling/sc/page.py
@@ -16,26 +16,54 @@ class Page:
     This class saves the details of an institution's seasonal component model.
     """
 
-    def __init__(self):
-        """
-        Constructor
-        """
-
-        self.__configurations = config.Config()
-
-    def exc(self, system: tfc.STLForecastResults, code: ce.Codes):
+    def __init__(self, system: tfc.STLForecastResults, code: ce.Codes):
         """
 
         :param system: The results of the seasonal component model
         :param code: The identification code of an institution
+        """
+
+        self.__system = system
+        self.__code = code
+
+        configurations = config.Config()
+        self.__root = os.path.join(configurations.artefacts_, 'models', code.hospital_code)
+
+    def __latex(self):
+        """
+
         :return:
         """
 
-        pathstr = os.path.join(self.__configurations.artefacts_, 'models', code.hospital_code, 'scf.txt')
+        pathstr = os.path.join(self.__root, 'scf.tex')
 
         try:
             with open(file=pathstr, mode='w', encoding='utf-8', newline='\r\n') as disk:
-                disk.write(system.summary().as_text())
-            logging.info('scf.txt: succeeded (%s)', code.hospital_code)
+                disk.write(self.__system.summary().as_latex())
+            logging.info('sfc.tex: succeeded (%s)', self.__code.hospital_code)
         except IOError as err:
             raise err from err
+
+    def __txt(self):
+        """
+
+        :return:
+        """
+
+        pathstr = os.path.join(self.__root, 'scf.txt')
+
+        try:
+            with open(file=pathstr, mode='w', encoding='utf-8', newline='\r\n') as disk:
+                disk.write(self.__system.summary().as_text())
+            logging.info('scf.txt: succeeded (%s)', self.__code.hospital_code)
+        except IOError as err:
+            raise err from err
+
+    def exc(self):
+        """
+
+        :return:
+        """
+
+        self.__txt()
+        self.__latex()
