68a467a5fb58e630433e6882a07dbf7dae80f674
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-06 20:49:35 +0000

    Notification: src/modelling/sc/algorithm.py -> Testing State

--- src/modelling/sc/algorithm.py
+++ src/modelling/sc/algorithm.py
@@ -3,6 +3,9 @@ import logging
 import pandas as pd
 import statsmodels.tsa.arima.model as tar
 import statsmodels.tsa.forecasting.stl as tfc
+import statsmodels.tools.sm_exceptions as sme
+
+import src.elements.codes as ce
 
 
 class Algorithm:
@@ -19,7 +22,7 @@ class Algorithm:
         self.__arguments: dict = arguments
         self.__sc: dict = self.__arguments.get('sc')
 
-    def exc(self, training: pd.DataFrame) -> tfc.STLForecastResults:
+    def exc(self, training: pd.DataFrame, code: ce.Codes) -> tfc.STLForecastResults | None:
         """
         <b>References</b><br>
         <a href="https://www.statsmodels.org/dev/generated/statsmodels.tsa.arima.model.ARIMA.html">
@@ -27,6 +30,7 @@ class Algorithm:
         <a href="https://www.statsmodels.org/dev/generated/statsmodels.tsa.forecasting.stl.STLForecast.html">STLForecast</a><br><br>
 
         :param training: The data of an institution, including the decompositions of its <i>ln(attendance numbers)</i> series.
+        :param code:
         :return:
         """
 
@@ -35,22 +39,27 @@ class Algorithm:
 
         training.index.freq = self.__arguments.get('frequency')
 
-        architecture: tfc.STLForecast
-        architecture = tfc.STLForecast(
-            training[['seasonal']], tar.ARIMA,
-            model_kwargs=dict(
-                seasonal_order=(
-                    self.__sc.get('P'),
-                    self.__sc.get('D'),
-                    self.__sc.get('Q'),
-                    self.__sc.get('m')),
-                trend='c'),
-            seasonal=self.__sc.get('smoother_seasonal'),
-            seasonal_deg=self.__sc.get('degree_seasonal'),
-            trend_deg=self.__sc.get('degree_trend'),
-            robust=False)
+        try:
+            architecture: tfc.STLForecast
+            architecture = tfc.STLForecast(
+                training[['seasonal']], tar.ARIMA,
+                model_kwargs=dict(
+                    seasonal_order=(
+                        self.__sc.get('P'),
+                        self.__sc.get('D'),
+                        self.__sc.get('Q'),
+                        self.__sc.get('m')),
+                    trend='c'),
+                seasonal=self.__sc.get('smoother_seasonal'),
+                seasonal_deg=self.__sc.get('degree_seasonal'),
+                trend_deg=self.__sc.get('degree_trend'),
+                robust=False)
+        except sme.ConvergenceWarning as err:
+            logging.info('%s (%s)', err, code.hospital_code)
+            return None
+
 
         system: tfc.STLForecastResults
         system = architecture.fit()
-        
+
         return system
