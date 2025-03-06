2dc13d859d6063425f9f5f9d7c8e18cfe996a697
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-06 11:52:33 +0000

    Update src/modelling/sc/forecasts.py: In Progress

--- src/modelling/sc/forecasts.py
+++ src/modelling/sc/forecasts.py
@@ -95,6 +95,6 @@ class Forecasts:
 
         message = self.__objects.write(
             nodes=nodes,
-            path=os.path.join(self.__configurations.artefacts_, 'models', code.hospital_code, 'sfc.json'))
+            path=os.path.join(self.__configurations.artefacts_, 'models', code.hospital_code, 'scf.json'))
 
         logging.info(message)
