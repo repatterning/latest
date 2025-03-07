173a553fa56958bb369f1d5443b809d718d6aa53
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-07 17:17:03 +0000

    Update src/modelling/sc/algorithm.py

--- src/modelling/sc/algorithm.py
+++ src/modelling/sc/algorithm.py
@@ -1,13 +1,12 @@
 """Module algorithm.py"""
 import logging
-import warnings
 
 import pandas as pd
-import statsmodels.tsa.arima.model as tar
 import statsmodels.tsa.forecasting.stl as tfc
-import statsmodels.tools.sm_exceptions as sme
 
 import src.elements.codes as ce
+import src.modelling.sc.fundamental
+import src.modelling.sc.seasonal
 
 
 class Algorithm:
@@ -23,49 +22,32 @@ class Algorithm:
 
         self.__arguments: dict = arguments
 
-        # Seasonal Components Arguments
-        self.__sc: dict = self.__arguments.get('sc')
-
     def exc(self, training: pd.DataFrame, code: ce.Codes) -> tfc.STLForecastResults | None:
         """
         <b>References</b><br>
         <a href="https://www.statsmodels.org/dev/generated/statsmodels.tsa.arima.model.ARIMA.html">
         ARIMA (Autoregressive Integrated Moving Average)</a><br>
-        <a href="https://www.statsmodels.org/dev/generated/statsmodels.tsa.forecasting.stl.STLForecast.html">STLForecast</a><br><br>
+        <a href="https://www.statsmodels.org/stable/generated/statsmodels.tsa.statespace.sarimax.SARIMAX.html">
+        SARIMAX (Seasonal AutoRegressive Integrated Moving Average with eXogenous)</a><br>
+        <a href="https://www.statsmodels.org/dev/generated/statsmodels.tsa.forecasting.stl.STLForecast.html">
+        STLForecast</a><br><br>
 
-        :param training: The data of an institution, including the decompositions of its <i>ln(attendance numbers)</i> series.
-        :param code:
+        :param training: The data of an institution, including the decompositions of its <i>ln(attendance numbers)</i> series.<br>
+        :param code: The health board & institution/hospital codes of an institution/hospital.<br>
         :return:
         """
 
-        frequency = training.index.inferred_freq
-        logging.info(frequency)
+        logging.info('Modelling %s', code.hospital_code)
 
+        # Setting data frequency
         training.index.freq = self.__arguments.get('frequency')
 
+        # Modelling
+        system: tfc.STLForecastResults = src.modelling.sc.fundamental.Fundamental(
+            training=training, arguments=self.__arguments).exc()
 
-        architecture: tfc.STLForecast
-        architecture = tfc.STLForecast(
-            training[['seasonal']], tar.ARIMA,
-            model_kwargs=dict(
-                seasonal_order=(
-                    self.__sc.get('P'),
-                    self.__sc.get('D'),
-                    self.__sc.get('Q'),
-                    self.__sc.get('m')),
-                trend='c'),
-            seasonal=self.__sc.get('smoother_seasonal'),
-            seasonal_deg=self.__sc.get('degree_seasonal'),
-            trend_deg=self.__sc.get('degree_trend'),
-            robust=False)
-
-        # warnings.filterwarnings(action='error', category=sme.ConvergenceWarning)
-        try:
-            system: tfc.STLForecastResults
-            system = architecture.fit(fit_kwargs={'method': 'statespace', 'cov_type': 'robust'})
-        except RuntimeWarning as err:
-            logging.info('Problem: %s', code.hospital_code)
-            return None
-            # raise err from err
+        if system is None:
+            system: tfc.STLForecastResults = src.modelling.sc.seasonal.Seasonal(
+                training=training, arguments=self.__arguments).exc()
 
         return system
