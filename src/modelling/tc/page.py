392e8fb38fb0a61e06527b088507ba45a40e66e3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-08 21:28:32 +0000

    Update src/modelling/tc/page.py: In Progress

--- src/modelling/tc/page.py
+++ src/modelling/tc/page.py
@@ -1,13 +1,23 @@
-
+import os
 import pymc
+
+import config
 import src.elements.codes as ce
 
 class Page:
 
     def __init__(self, model: pymc.model.Model, code: ce.Codes):
+        """
+        
+        :param model:
+        :param code:
+        """
 
         self.__model = model
         self.__code = code
+
+        configurations = config.Config()
+        self.__root = os.path.join(configurations.artefacts_, 'models', code.hospital_code)
 
     def __graph(self):
         pass
