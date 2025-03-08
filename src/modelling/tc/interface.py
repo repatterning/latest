25b880df9ce0b948f8bfb03f6dd1aea7782f3697
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-08 20:52:58 +0000

    Update src/modelling/tc/interface.py: In Progress

--- src/modelling/tc/interface.py
+++ src/modelling/tc/interface.py
@@ -1,19 +1,26 @@
 
 import src.elements.codes as ce
+import src.elements.master as mr
+
+import src.modelling.tc.algorithm
+
 
 class Interface:
 
-    def __init__(self):
-        pass
+    def __init__(self, arguments: dict):
 
-    @staticmethod
-    def exc(code: ce.Codes, state: bool) -> str:
+        self.__arguments = arguments
+
+    def exc(self, master: mr.Master, code: ce.Codes, state: bool) -> str:
         """
 
+        :param master
         :param code:
         :param state:
         :return:
         """
+
+        algorithm = src.modelling.tc.algorithm.Algorithm(training=master.training, arguments=self.__arguments)
 
         if state:
             return f'Trend Component Modelling: Proceed -> {code.hospital_code}'
