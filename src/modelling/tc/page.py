53fc3067c5b9c50056ff4addd974dab6d63b86b8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-08 21:27:38 +0000

    Update src/modelling/tc/page.py: In Progress

--- src/modelling/tc/page.py
+++ src/modelling/tc/page.py
@@ -1,8 +1,13 @@
 
+import pymc
+import src.elements.codes as ce
+
 class Page:
 
-    def __init__(self):
-        pass
+    def __init__(self, model: pymc.model.Model, code: ce.Codes):
+
+        self.__model = model
+        self.__code = code
 
     def __graph(self):
         pass
