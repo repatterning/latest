d675650e0a36cd5271f060a29637422b36b6a78a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-09 13:13:09 +0000

    Update src/modelling/tc/interface.py: In Progress

--- src/modelling/tc/interface.py
+++ src/modelling/tc/interface.py
@@ -16,12 +16,16 @@ class Interface:
     def exc(self, master: mr.Master, code: ce.Codes, state: bool) -> str:
         """
 
-        :param master: The training & testing data.
+        :param master: The training & testing data of an institution.
         :param code: The health board & institution/hospital codes of an institution/hospital.
         :param state: If the seasonal component modelling step was a success, this will be True; otherwise False.
         :return:
         """
 
+        if not state:
+            return f'Trend Component Modelling: Skip -> {code.hospital_code}'
+
+        # Determine
         algorithm = src.modelling.tc.algorithm.Algorithm(training=master.training, arguments=self.__arguments)
         model, gp, details = algorithm.exc()
 
@@ -31,7 +35,5 @@ class Interface:
         logging.info(type(gp))
         logging.info(type(details))
 
-        if state:
-            return f'Trend Component Modelling: Proceed -> {code.hospital_code}'
 
-        return f'Trend Component Modelling: Skip -> {code.hospital_code}'
+        return f'Trend Component Modelling: Success -> {code.hospital_code}'
