292482e9865bb234228e1c1744189613c03f2794
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-09 13:51:28 +0000

    Update src/modelling/tc/conditionals.py: In Progress

--- src/modelling/tc/conditionals.py
+++ src/modelling/tc/conditionals.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 import pymc
 import arviz
@@ -5,10 +6,25 @@ import arviz
 
 class Conditionals:
 
-    def __init__(self, gp: pymc.gp.Marginal, details: arviz.InferenceData):
+    def __init__(self, gp: pymc.gp.Marginal, details: arviz.InferenceData, abscissae: np.ndarray):
 
         self.__gp = gp
         self.__details = details
+        self.__abscissae = abscissae
 
-    def exc(self):
+    def __free(self, model: pymc.model.Model):
+
+        with model:
+            self.__gp.conditional('estimates', self.__abscissae, pred_noise=False)
+            predictions = pymc.sample_posterior_predictive(
+                self.__details, var_names=['estimates'])
+
+    def __noisy(self, model: pymc.model.Model):
+
+        with model:
+            self.__gp.conditional('n_estimates', self.__abscissae, pred_noise=True)
+            n_predictions = pymc.sample_posterior_predictive(
+                self.__details, var_names=['n_estimates'])
+
+    def exc(self, model: pymc.model.Model):
         pass
