b683db6e512795044a7545817e533bb33ac9a14f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-09 17:18:28 +0000

    Update src/modelling/tc/forecasting.py: In Progress

--- src/modelling/tc/forecasting.py
+++ src/modelling/tc/forecasting.py
@@ -1,18 +1,26 @@
 import logging
+import os
 
 import numpy as np
 
 import pymc
 import arviz
 
+import config
+import src.elements.codes as ce
+
 
 class Forecasting:
 
-    def __init__(self, gp: pymc.gp.Marginal, details: arviz.InferenceData, abscissae: np.ndarray):
+    def __init__(self, gp: pymc.gp.Marginal, details: arviz.InferenceData, abscissae: np.ndarray, code: ce.Codes):
 
         self.__gp = gp
         self.__details = details
         self.__abscissae = abscissae
+        self.__code = code
+
+        configurations = config.Config()
+        self.__root = os.path.join(configurations.artefacts_, 'models', code.hospital_code)
 
     def __execute(self, name: str, model_: pymc.model.Model, pred_noise: bool):
 
@@ -22,21 +30,24 @@ class Forecasting:
 
         return model_, objects
 
-    def exc(self, model: pymc.model.Model, execute_observation_noise_option: bool):
+    def __inferences(self, data: arviz.InferenceData, name: str):
+
+        pathstr = os.path.join(self.__root, name)
+
+        try:
+            data.to_netcdf(filename=pathstr)
+        except IOError as err:
+            raise err from err
+
+    def exc(self, model: pymc.model.Model):
         """
 
         :param model:
-        :param execute_observation_noise_option:
         :return:
         """
 
-
         model, predictions = self.__execute(name='estimates', model_=model, pred_noise=False)
-        logging.info('Persist\n%s', predictions)
-
-        if execute_observation_noise_option:
-            model, n_predictions = self.__execute(name='n_estimates', model_=model, pred_noise=True)
-            logging.info('Persist\n%s',n_predictions)
+        model, n_predictions = self.__execute(name='n_estimates', model_=model, pred_noise=True)
 
         # Persist
         logging.info(model)
