6758294e69b4014b8ebba34039126dcc14f0091c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-10 23:54:48 +0000

    Update src/modelling/core.py: In Progress

--- src/modelling/core.py
+++ src/modelling/core.py
@@ -1,9 +1,72 @@
+import logging
+import os
+import glob
+import dask
+import pandas as pd
+
+import config
+import src.elements.codes as ce
+import src.functions.streams
+import src.elements.text_attributes as txa
+import src.modelling.tc.interface
 
 
 class Core:
 
-    def __init__(self):
-        pass
+    def __init__(self, codes: list[ce.Codes], arguments: dict):
+        """
+        
+        :param codes: 
+        :param arguments: 
+        """
 
-    def exc(self):
-        pass
+        self.__codes = codes
+        self.__arguments = arguments
+
+        self.__configurations = config.Config()
+        self.__streams = src.functions.streams.Streams()
+        
+    def __get_codes(self) -> list[ce.Codes]:
+        """
+
+        :return:
+        """
+
+        strings = glob.glob(
+            pathname=os.path.join(self.__configurations.artefacts_, 'data', '**', '*training.csv'))
+        values = [os.path.basename(os.path.dirname(string)) for string in strings]
+        
+        return [code for code in self.__codes if code.hospital_code in values]
+
+    @dask.delayed
+    def __get_training_data(self, code: ce.Codes) -> pd.DataFrame:
+        """
+
+        :param code:
+        :return:
+        """
+
+        uri = os.path.join(self.__configurations.artefacts_, 'data', code.hospital_code, 'training.csv')
+        text = txa.TextAttributes(uri=uri, header=0)
+        
+        return self.__streams.read(text=text)
+
+    def exc(self) -> list[str]:
+        """
+
+        :return:
+        """
+
+        tc = dask.delayed(src.modelling.tc.interface.Interface(arguments=self.__arguments).exc)
+
+        codes = self.__get_codes()
+
+        computations = []
+        for code in codes:            
+            training = self.__get_training_data(code=code)
+            message = tc(training=training, code=code)
+            computations.append(message)
+
+        messages = dask.compute(computations, num_workers=2)[0]
+
+        return messages
