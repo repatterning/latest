f96af0388ade65348e3f4bc2b9792a5d81976370
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-10 23:14:32 +0000

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ class Config:
         now = datetime.datetime.now()
         offset = (now.weekday() - 1) % 7
         tuesday = now - datetime.timedelta(days=offset)
-        
+
         logging.info(tuesday.strftime('%Y-%m-%d'))
         self.stamp: str = '2025-03-04'
         logging.info(self.stamp)
