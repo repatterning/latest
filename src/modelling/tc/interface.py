da3bf4526d476f13865f71c0959511843ece34a2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-11 18:25:29 +0000

    Update src/modelling/tc/interface.py: In Progress

--- src/modelling/tc/interface.py
+++ src/modelling/tc/interface.py
@@ -1,16 +1,12 @@
 """Module interface.py"""
 import logging
-import typing
 
-import arviz
 import numpy as np
 import pandas as pd
-import pymc
 
 import src.elements.codes as ce
 import src.modelling.tc.algorithm
 import src.modelling.tc.forecasting
-import src.modelling.tc.page
 
 
 class Interface:
@@ -23,37 +19,29 @@ class Interface:
 
         self.__arguments = arguments
 
-    def __trend_component_modelling(self, training: pd.DataFrame) -> (
-            typing.Tuple)[pymc.model.Model, pymc.gp.Marginal, arviz.InferenceData]:
-        """
-
-        :param training: The training data of an institution.
-        :return:
-        """
-
-        algorithm = src.modelling.tc.algorithm.Algorithm(training=training, arguments=self.__arguments)
-
-        return algorithm.exc()
-
-    def exc(self, training: pd.DataFrame, code: ce.Codes) -> str:
+    def exc(self, training: pd.DataFrame, code: ce.Codes, state: bool) -> str:
         """
 
         :param training: The training data of an institution.
         :param code: The health board & institution/hospital codes of an institution/hospital.
+        :param state:
         :return:
         """
 
-        # Model
-        model, gp, details = self.__trend_component_modelling(training=training)
+        if state:
 
-        # Persist: Algorithm
-        src.modelling.tc.page.Page(model=model, code=code).exc(label='algorithm')
+            # Model, etc.
+            model, gp, details  = src.modelling.tc.algorithm.Algorithm(
+                training=training, arguments=self.__arguments).exc()
 
-        # Estimates & Futures
-        abscissae = np.arange(
-            training.shape[0] + (2 * self.__arguments.get('ahead'))
-        )[:, None]
-        src.modelling.tc.forecasting.Forecasting(
-            gp=gp, details=details, abscissae=abscissae, code=code).exc(model=model)
+            # Estimates & Futures
+            abscissae = np.arange(
+                training.shape[0] + (2 * self.__arguments.get('ahead'))
+            )[:, None]
+            logging.info('%s\n%s', abscissae.shape, abscissae)
+            src.modelling.tc.forecasting.Forecasting(
+                gp=gp, details=details, abscissae=abscissae, code=code).exc(model=model)
 
-        return f'Trend Component Modelling: Success -> {code.hospital_code}'
+            return f'Trend Component Modelling: Success -> {code.hospital_code}'
+
+        return f'Skip: {code.hospital_code}'
