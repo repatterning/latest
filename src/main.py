5c614e55b4806e5ec04c952d60d79fa11e8ef40d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-11 18:39:24 +0000

    Update src/main.py: In Progress

--- src/main.py
+++ src/main.py
@@ -4,8 +4,6 @@ import os
 import sys
 
 import boto3
-import jax
-import numpyro
 import pytensor
 
 
@@ -16,11 +14,7 @@ def main():
     """
 
     logger: logging.Logger = logging.getLogger(__name__)
-    logger.info('The number of GPU devices: %s', jax.device_count(backend='gpu'))
-    logger.info('The number of CPU devices: %s', jax.device_count(backend='cpu'))
-    logger.info('The default device (depends on the jax.config.update setting): %s', jax.local_devices()[0])
-    logger.info('GPU: %s', str(jax.local_devices()[0]).startswith('cuda'))
-    logging.info('BLAS: %s', pytensor.config.blas__ldflags)
+    logger.info('BLAS: %s', pytensor.config.blas__ldflags)
 
     # Setting up
     src.setup.Setup(service=service, s3_parameters=s3_parameters).exc()
@@ -30,10 +24,11 @@ def main():
 
     # Modelling
     src.modelling.interface.Interface(
-        data=data, arguments=arguments).exc()
+       data=data, arguments=arguments).exc()
 
     # Transfer
-    src.transfer.interface.Interface(connector=connector, service=service, s3_parameters=s3_parameters).exc()
+    src.transfer.interface.Interface(
+       connector=connector, service=service, s3_parameters=s3_parameters).exc()
 
     # Cache
     src.functions.cache.Cache().exc()
@@ -45,15 +40,22 @@ if __name__ == '__main__':
     sys.path.append(root)
     sys.path.append(os.path.join(root, 'src'))
 
+
     # Logging
     logging.basicConfig(level=logging.INFO,
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
     # Classes
+    import environment
+    import src.data.interface
+    import src.functions.cache
     import src.functions.service
+    import src.modelling.interface
     import src.s3.s3_parameters
     import src.s3.configurations
+    import src.setup
+    import src.transfer.interface
 
     # Vis-à-vis Amazon & Development: Connector, S3 Parameters, Platform Services, Configurations
     connector = boto3.session.Session()
@@ -64,22 +66,7 @@ if __name__ == '__main__':
 
     # Environment Variables
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-    os.environ['XLA_FLAGS'] = (
-        '--xla_disable_hlo_passes=constant_folding '
-    )
-    os.environ['OMP_NUM_THREADS'] = "1"
     pytensor.config.blas__ldflags = '-llapack -lblas -lcblas'
-    jax.config.update('jax_platform_name', 'gpu')
-    jax.config.update('jax_enable_x64', False)
-    # numpyro.set_host_device_count(12 if arguments.get('device') == 'cpu' else jax.device_count(backend='gpu'))
-    numpyro.set_platform('gpu')
-
-    # Classes
-    import src.data.interface
-    import src.functions.cache
-    import src.modelling.core
-    import src.modelling.interface
-    import src.setup
-    import src.transfer.interface
+    environment.Environment().__call__(arguments=arguments)
 
     main()
