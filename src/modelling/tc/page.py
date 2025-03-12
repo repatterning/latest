c40ceb05f26174ee41a26338829c4276a35c934e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-12 12:12:23 +0000

    Update src/modelling/tc/page.py

--- src/modelling/tc/page.py
+++ src/modelling/tc/page.py
@@ -1,11 +1,8 @@
 """Module page.py"""
-import os
 import logging
+import os
 
 import pymc
-
-import config
-import src.elements.codes as ce
 
 
 class Page:
@@ -16,18 +13,15 @@ class Page:
     This class saves the details of an institution's trend component model.
     """
 
-    def __init__(self, model: pymc.model.Model, code: ce.Codes):
+    def __init__(self, model: pymc.model.Model, path: str):
         """
 
         :param model: The trend component model of an institution.
-        :param code: The health board & institution/hospital codes of an institution/hospital.
+        :param path:
         """
 
         self.__model = model
-        self.__code = code
-
-        configurations = config.Config()
-        self.__root = os.path.join(configurations.artefacts_, 'models', code.hospital_code)
+        self.__path = path
 
     def __graph(self, label: str):
         """
@@ -36,12 +30,12 @@ class Page:
         :return: 
         """
 
-        pathstr = os.path.join(self.__root, f'tcf_{label}.pdf')
+        pathstr = os.path.join(self.__path, f'tcf_{label}.pdf')
 
         try:
             pymc.model_graph.model_to_graphviz(
                 model=self.__model, figsize=(2, 2), save=pathstr, dpi=1200)
-            logging.info('%s: %s', self.__code.hospital_code, os.path.basename(pathstr))
+            logging.info('%s: %s', os.path.basename(self.__path), os.path.basename(pathstr))
         except IOError as err:
             raise err from err
 
@@ -52,12 +46,12 @@ class Page:
         :return: 
         """
 
-        pathstr = os.path.join(self.__root, f'tcf_{label}.txt')
+        pathstr = os.path.join(self.__path, f'tcf_{label}.txt')
 
         try:
             with open(file=pathstr, mode='w', encoding='utf-8', newline='\r\n') as disk:
                 disk.write(self.__model.str_repr())
-            logging.info('%s: %s', self.__code.hospital_code, os.path.basename(pathstr))
+            logging.info('%s: %s', os.path.basename(self.__path), os.path.basename(pathstr))
         except IOError as err:
             raise err from err
 
