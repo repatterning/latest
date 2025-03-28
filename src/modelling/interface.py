42e6736b81e82c549634b63a3a05a30d20083040
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-28 14:38:12 +0000

    Update src/modelling/interface.py: In Progress

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -1,31 +1,38 @@
 """Module interface.py"""
-
+import logging
 import pandas as pd
+import dask
 
 import config
 import src.functions.directories
 import src.modelling.codes
 import src.modelling.core
-
+import src.modelling.data
 
 class Interface:
     """
     The interface to the seasonal & trend component modelling steps.
     """
 
-    def __init__(self, data: pd.DataFrame, arguments: dict):
+    def __init__(self, assets: pd.DataFrame, arguments: dict):
         """
 
-        :param data: For reading ...
+        :param assets: Of assets
         :param arguments: A set of model development, and supplementary, arguments.
         """
 
-        self.__data = data
+        self.__assets = assets[:8]
         self.__arguments = arguments
 
         # Instances
         self.__configurations = config.Config()
         self.__directories = src.functions.directories.Directories()
+
+    @dask.delayed
+    def __get_sections(self, ts_id: int) -> list:
+
+        return self.__assets.loc[
+            self.__assets['ts_id'] == ts_id, 'uri'].to_list()
 
     def exc(self):
         """
@@ -34,3 +41,16 @@ class Interface:
 
         :return:
         """
+        
+        __get_data = dask.delayed(src.modelling.data.Data().exc)
+        
+        
+        computations = []
+        for ts_id in self.__assets['ts_id'].unique():
+            
+            sections = self.__get_sections(ts_id=ts_id)
+            data = __get_data(sections=sections)
+            computations.append(...)
+            
+        calculations = dask.compute(computations, scheduler='threads')[0]
+        logging.info(calculations)
