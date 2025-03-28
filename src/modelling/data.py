7b0ea8a51e20acb757e31ec45f25943385fe7a01
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-28 22:12:07 +0000

    Update src/modelling/data.py

--- src/modelling/data.py
+++ src/modelling/data.py
@@ -1,26 +1,43 @@
+"""Module data.py"""
 import datetime
 import time
-
 
 import dask.dataframe as ddf
 import pandas as pd
 
 
 class Data:
+    """
+    A gauge's data
+    """
 
     def __init__(self, arguments: dict):
+        """
+
+        :param arguments:
+        """
 
         spanning = arguments.get('spanning')
         as_from = datetime.date.today() - datetime.timedelta(days=round(spanning*365))
         self.__starting = 1000 * time.mktime(as_from.timetuple())
 
     def __setting_up(self, frame: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        :param frame:
+        :return:
+        """
 
         frame['date'] = pd.to_datetime(frame['timestamp'], unit='ms')
 
         return frame.loc[frame['timestamp'] >= self.__starting, :]
 
     def exc(self, sections: list) -> pd.DataFrame:
+        """
+
+        :param sections:
+        :return:
+        """
 
         try:
             data = ddf.read_csv(urlpath=sections)
