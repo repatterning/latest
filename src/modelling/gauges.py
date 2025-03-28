4bca08eaaec90963bedf5ad4f2cb1afb3574e7fa
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-28 14:57:34 +0000

    Add src/modelling/gauges.py

---
+++ src/modelling/gauges.py
@@ -0,0 +1,36 @@
+"""Module codes.py"""
+import pandas as pd
+
+import src.elements.gauge as ge
+
+
+class Gauges:
+    """
+    Determines the unique set of health board & institution pairings
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def __structure(values: list[dict]) -> list[ge.Gauge]:
+        """
+
+        :param values:
+        :return:
+        """
+
+        return [ge.Gauge(**value) for value in values]
+
+    def exc(self, data: pd.DataFrame) -> list[ge.Gauge]:
+        """
+
+        :param data:
+        :return:
+        """
+
+        # Codes
+        frame = data[['catchment_id', 'ts_id']].drop_duplicates()
+        values: list[dict] = frame.reset_index(drop=True).to_dict(orient='records')
+
+        return self.__structure(values=values)
