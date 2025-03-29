d02b77134086dc2993a082e94996c18e9f11db9d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-29 23:14:29 +0000

    Update src/modelling/data.py

--- src/modelling/data.py
+++ src/modelling/data.py
@@ -23,6 +23,15 @@ class Data:
         as_from = datetime.date.today() - datetime.timedelta(days=round(spanning*365))
         self.__starting = 1000 * time.mktime(as_from.timetuple())
 
+    @staticmethod
+    def __anomalies(frame: pd.DataFrame) -> pd.DataFrame:
+
+        data = frame.copy().drop(columns='quality_code')
+        data.sort_values(by='timestamp', ascending=True, inplace=True)
+        data.drop_duplicates(keep='last', inplace=True)
+
+        return data
+
     def __setting_up(self, frame: pd.DataFrame) -> pd.DataFrame:
         """
 
@@ -33,6 +42,23 @@ class Data:
         frame['date'] = pd.to_datetime(frame['timestamp'], unit='ms')
 
         return frame.loc[frame['timestamp'] >= self.__starting, :]
+
+    @staticmethod
+    def __timings(frame: pd.DataFrame):
+        """
+
+        :param frame:
+        :return:
+        """
+
+        dates = pd.date_range(start=frame['date'].min(), end=frame['date'].max(),
+                              freq='h', inclusive='left')
+        slices = pd.DataFrame(data={'date': dates})
+
+        data = slices.merge(frame, how='left', on='date')
+        data.sort_values(by=['ts_id', 'timestamp'], ascending=True, inplace=True)
+
+        return data
 
     def exc(self, sections: list, gauge: ge.Gauge) -> pd.DataFrame:
         """
@@ -48,7 +74,15 @@ class Data:
             raise err from err
 
         frame: pd.DataFrame = data.compute()
-        frame = self.__setting_up(frame=frame.copy())
+
+        # Anomalies
+        frame = self.__anomalies(frame=frame)
+
+        # Measure
         frame['measure'] = frame['value'] + gauge.gauge_datum
+
+        # Setting up, focusing on hour points
+        frame = self.__setting_up(frame=frame.copy())
+        frame = self.__timings(frame=frame.copy())
 
         return frame
