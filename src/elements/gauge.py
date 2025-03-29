4b88d9ed70eddcea08cbba78a58fe955ebd88ba7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-29 17:01:44 +0000

    Update src/elements/gauge.py

--- src/elements/gauge.py
+++ src/elements/gauge.py
@@ -11,10 +11,13 @@ class Gauge(typing.NamedTuple):
     <b>ts_id</b> : int
         The identification code of a gauge's time series<br>
 
-    <b>catchment_id</b> : str
+    <b>catchment_id</b> : int
         The catchment identification code of the gauge's catchment area.
 
+    <b>gauge_datum</b> : float
+        The reference point of a gauge site, in metres.
     """
 
     ts_id: int
     catchment_id: int
+    gauge_datum: float
