bb738c25d11f44ab673805be45ee8a99f48f8a31
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-30 00:11:50 +0000

    Update src/modelling/architecture/page.py

--- src/modelling/architecture/page.py
+++ src/modelling/architecture/page.py
@@ -1,12 +1,9 @@
 """Module page.py"""
 import logging
 import os
-import pathlib
 
 import statsmodels.tsa.forecasting.stl as tfs
 
-import config
-import src.elements.gauge as ge
 import src.functions.objects
 
 
@@ -18,19 +15,16 @@ class Page:
     This class saves the details of an institution's seasonal component model.
     """
 
-    def __init__(self, system: tfs.STLForecastResults, gauge: ge.Gauge):
+    def __init__(self, system: tfs.STLForecastResults, path: str):
         """
 
         :param system: The forecasts/predictions of the seasonal component model.
-        :param gauge: Encodes the time series & catchment identification codes of a gauge, and its gauge datum.<br>
+        :param path: Storage path<br>
         """
 
         self.__system = system
-        self.__gauge = gauge
 
-        configurations = config.Config()
-        self.__root = os.path.join(configurations.artefacts_, 'models',
-                                   str(self.__gauge.catchment_id), str(self.__gauge.ts_id))
+        self.__path = path
 
     def __latex(self):
         """
@@ -38,13 +32,12 @@ class Page:
         :return:
         """
 
-        pathstr = os.path.join(self.__root, 'scf_measures.tex')
+        pathstr = os.path.join(self.__path, 'measures.tex')
 
         try:
             with open(file=pathstr, mode='w', encoding='utf-8', newline='\r\n') as disk:
                 disk.write(self.__system.summary().as_latex())
-            logging.info('%s: succeeded (%s of %s)', pathlib.PurePath(pathstr).name,
-                         self.__gauge.catchment_id, self.__gauge.ts_id)
+            logging.info('success: %s', pathstr)
         except IOError as err:
             raise err from err
 
@@ -54,13 +47,12 @@ class Page:
         :return:
         """
 
-        pathstr = os.path.join(self.__root, 'scf_measures.txt')
+        pathstr = os.path.join(self.__path, 'measures.txt')
 
         try:
             with open(file=pathstr, mode='w', encoding='utf-8', newline='\r\n') as disk:
                 disk.write(self.__system.summary().as_text())
-            logging.info('%s: succeeded (%s of %s)', pathlib.PurePath(pathstr).name,
-                         self.__gauge.catchment_id, self.__gauge.ts_id)
+            logging.info('success: %s', pathstr)
         except IOError as err:
             raise err from err
 
@@ -76,7 +68,7 @@ class Page:
         }
 
         # Path
-        pathstr = os.path.join(self.__root, 'scf_extra.json')
+        pathstr = os.path.join(self.__path, 'extra.json')
 
         # Persist
         message = src.functions.objects.Objects().write(nodes=nodes, path=pathstr)
