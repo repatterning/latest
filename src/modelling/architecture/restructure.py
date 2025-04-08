8dd4383d66a03a1fb080af640fb6fea2e55de8e5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-08 13:13:42 +0100

    Update src/modelling/architecture/restructure.py

--- src/modelling/architecture/restructure.py
+++ src/modelling/architecture/restructure.py
@@ -1,16 +1,24 @@
-
+"""Module restructure.py"""
 import json
 
 import pandas as pd
 
 
 class Restructure:
+    """
+    Restructure
+    """
 
     def __init__(self):
         pass
 
     @staticmethod
     def exc(data: pd.DataFrame) -> dict:
+        """
+
+        :param data: A frame of estimates
+        :return:
+        """
 
         string = data.to_json(orient='split')
         dictionary = json.loads(string)
