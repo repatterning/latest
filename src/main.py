88f73e4da920e09c3a7e940273740268bc0419f1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-11 02:12:56 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -22,8 +22,8 @@ def main():
     src.modelling.interface.Interface(listings=listings, arguments=arguments).exc(partitions=partitions)
 
     # Transfer
-    # src.transfer.interface.Interface(
-    #    connector=connector, service=service, s3_parameters=s3_parameters).exc()
+    src.transfer.interface.Interface(
+       connector=connector, service=service, s3_parameters=s3_parameters).exc()
 
     # Cache
     src.functions.cache.Cache().exc()
